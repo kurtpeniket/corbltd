13a28b124325b59284f49f82c3a4037346c48f42
Kurt Peniket <kurt.peniket@gmail.com>
2021-03-25 19:32:23 +0000

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'services', to: 'pages#services'
+  get 'commitments', to: 'pages#commitments'
+  get 'blog', to: 'pages#blog'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
