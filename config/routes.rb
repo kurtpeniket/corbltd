196aec19c837455f34584b628f842b618098aedb
Kurt Peniket <kurt.peniket@gmail.com>
2021-03-26 14:11:21 +0000

    new styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'services', to: 'pages#services'
-  get 'commitments', to: 'pages#commitments'
+  get 'projects', to: 'pages#projects'
+  get 'facilities', to: 'pages#facilities'
   get 'blog', to: 'pages#blog'
   get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
