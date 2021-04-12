87c0118460c7bb014e22ea614946796c2e54b9dc
Kurt Peniket <kurt.peniket@gmail.com>
2021-04-12 20:04:01 +0100

    mailer fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
-class Contact < MailForm::Base
-  attribute :name, :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
-  attribute :nickname,  :captcha  => true
+# class Contact < MailForm::Base
+#   attribute :name, :validate => true
+#   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+#   attribute :message,   :validate => true
+#   attribute :nickname,  :captcha  => true
 
-  def headers
-    {
-      :subject => "Contact Form",
-      :to => "kurt.peniket@gmail.com",
-      :from => %("#{name}" <#{email}>)
-    }
-  end
-end
+#   def headers
+#     {
+#       :subject => "Contact Form",
+#       :to => "kurt.peniket@gmail.com",
+#       :from => %("#{name}" <#{email}>)
+#     }
+#   end
+# end
