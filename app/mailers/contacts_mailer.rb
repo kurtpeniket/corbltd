b11d59b2c7276919917dc417739dbb6857a69c06
Kurt Peniket <kurt.peniket@gmail.com>
2021-04-12 19:58:12 +0100

    mailer fix

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -1,14 +1,14 @@
-class ContactForm < MailForm::Base
-  attributes :name,  :validate => true
-  attributes :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attributes :message
-  attribute :nickname,  :captcha  => true
+# class ContactForm < MailForm::Base
+#   attributes :name,  :validate => true
+#   attributes :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+#   attributes :message
+#   attribute :nickname,  :captcha  => true
 
-  def headers
-    {
-        :subject => "Your subject",
-        :to => "o6xc1r+etklgbjs4neyw@sharklasers.com",
-        :from => %("#{name}" <#{email}>)
-    }
-  end
-end
+#   def headers
+#     {
+#         :subject => "Your subject",
+#         :to => "o6xc1r+etklgbjs4neyw@sharklasers.com",
+#         :from => %("#{name}" <#{email}>)
+#     }
+#   end
+# end
